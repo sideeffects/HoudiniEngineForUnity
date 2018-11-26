1768f83f63dbeef17579c051a424632746648696
Seelan Vamatheva <seelanv@sidefx.com>
2018-11-26 13:01:11 -0500

    Fixed terrain and mesh inputs not working due to input scripts not getting registered on Editor startup.

--- Plugins/HoudiniEngineUnity/Scripts/Utility/HEU_InputInterfaceMesh.cs
+++ Plugins/HoudiniEngineUnity/Scripts/Utility/HEU_InputInterfaceMesh.cs
@@ -53,6 +53,7 @@ namespace HoudiniEngineUnity
 		/// Registers this input inteface for Unity meshes on
 		/// the callback after scripts are reloaded in Unity.
 		/// </summary>
+		[InitializeOnLoadMethod]
 		[UnityEditor.Callbacks.DidReloadScripts]
 		private static void OnScriptsReloaded()
 		{
