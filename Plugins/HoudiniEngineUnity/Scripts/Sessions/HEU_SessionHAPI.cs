bc8259d9020b28b2f8a2e13e964c77d0d57b15a1
Seelan Vamatheva <seelanv@sidefx.com>
2019-02-23 18:21:47 -0500

    For PDG GDC demo. Added convenience function to set parent transform with identify matrix. Added session-based error logging and error throwing overrides which allow session errors to be silenced while invoking HAPI calls that might result in errors that are expected (i.e. missing parms).

--- Plugins/HoudiniEngineUnity/Scripts/Sessions/HEU_SessionHAPI.cs
+++ Plugins/HoudiniEngineUnity/Scripts/Sessions/HEU_SessionHAPI.cs
@@ -642,9 +642,9 @@ namespace HoudiniEngineUnity
 
 				string statusMessage = GetStatusString(HAPI_StatusType.HAPI_STATUS_CALL_RESULT, HAPI_StatusVerbosity.HAPI_STATUSVERBOSITY_WARNINGS);
 				string errorMsg = string.Format("{0} : {1}\nIf session is invalid, try restarting Unity.", prependMsg, statusMessage);
-				SetSessionErrorMsg(errorMsg, bLogError); ;
+				SetSessionErrorMsg(errorMsg, LogErrorOverride && bLogError); ;
 
-				if (bThrowError)
+				if (ThrowErrorOverride && bThrowError)
 				{
 					throw new HEU_HoudiniEngineError(errorMsg);
 				}
