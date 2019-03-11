41492adf88abc178f75a7d6ef452a165b61ad5f7
Seelan Vamatheva <seelanv@sidefx.com>
2019-03-11 18:02:43 -0400

    Bumping to Houdini Version: 17.5.193 Houdini Engine Version: 3.2 (API: 40)

--- Plugins/HoudiniEngineUnity/Scripts/HEU_HoudiniVersion.cs
+++ Plugins/HoudiniEngineUnity/Scripts/HEU_HoudiniVersion.cs
@@ -41,10 +41,10 @@ namespace HoudiniEngineUnity
 	{
 		public const int HOUDINI_MAJOR			= 17;
 		public const int HOUDINI_MINOR			= 5;
-		public const int HOUDINI_BUILD			= 187;
+		public const int HOUDINI_BUILD			= 193;
 		public const int HOUDINI_PATCH			= 0;
 
-		public const string HOUDINI_VERSION_STRING = "17.5.187";
+		public const string HOUDINI_VERSION_STRING = "17.5.193";
 
 		public const int HOUDINI_ENGINE_MAJOR	= 3;
 		public const int HOUDINI_ENGINE_MINOR	= 2;
