f57f7db49ef37633efe82f0930bc2e0de17eabd5
Seelan Vamatheva <seelanv@sidefx.com>
2019-10-28 14:05:11 -0400

    Merged pr #9 from the plugin github repo which speeds up the slow (and sloppily written) input mesh uploading using a map for reindexing shared vertices. Merged pr #11 from the plugin github repo which fixes input mesh nested transform.

--- Plugins/HoudiniEngineUnity/Scripts/Utility/HEU_InputInterfaceMesh.cs
+++ Plugins/HoudiniEngineUnity/Scripts/Utility/HEU_InputInterfaceMesh.cs
@@ -184,24 +184,20 @@ namespace HoudiniEngineUnity
 			for (int i = 0; i < numMeshes; ++i)
 			{
 				Vector3[] meshVertices = inputDataMeshes._inputMeshes[i]._mesh.vertices;
-				Matrix4x4 localToWorld = inputDataMeshes._inputMeshes[i]._transform.localToWorldMatrix * rootInvertTransformMatrix;
+				Matrix4x4 localToWorld = rootInvertTransformMatrix * inputDataMeshes._inputMeshes[i]._transform.localToWorldMatrix;
 
 				List<Vector3> uniqueVertices = new List<Vector3>();
 
 				// Keep track of old vertex positions (old vertex slot points to new unique vertex slot)
 				int[] reindexVertices = new int[meshVertices.Length];
-
-				for (int j = 0; j < meshVertices.Length; ++j)
-				{
-					reindexVertices[j] = -1;
-				}
+				Dictionary<Vector3, int> reindexMap = new Dictionary<Vector3, int>();
 
 				// For each vertex, check against subsequent vertices for shared positions.
 				for (int a = 0; a < meshVertices.Length; ++a)
 				{
 					Vector3 va = meshVertices[a];
 
-					if (reindexVertices[a] == -1)
+					if (!reindexMap.ContainsKey(va))
 					{
 						if (numMeshes > 1 && !inputDataMeshes._hasLOD)
 						{
@@ -215,15 +211,11 @@ namespace HoudiniEngineUnity
 
 						// Reindex to point to unique vertex slot
 						reindexVertices[a] = uniqueVertices.Count - 1;
+						reindexMap[va] = uniqueVertices.Count - 1;
 					}
-
-					for (int b = a + 1; b < meshVertices.Length; ++b)
+					else
 					{
-						if (va == meshVertices[b])
-						{
-							// Shared vertex -> reindex to point to unique vertex slot
-							reindexVertices[b] = reindexVertices[a];
-						}
+						reindexVertices[a] = reindexMap[va];
 					}
 				}
 
