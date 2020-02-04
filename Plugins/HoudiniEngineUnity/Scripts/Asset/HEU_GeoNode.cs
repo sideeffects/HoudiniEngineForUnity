b6ea8e403e9a74db1c2cef9ce3f68938660b6e82
Seelan Vamatheva <seelanv@sidefx.com>
2020-02-04 14:36:43 -0500

    Fixed attribute instancing not keeping overrides or additional instance inputs on recook.

--- Plugins/HoudiniEngineUnity/Scripts/Asset/HEU_GeoNode.cs
+++ Plugins/HoudiniEngineUnity/Scripts/Asset/HEU_GeoNode.cs
@@ -260,13 +260,11 @@ namespace HoudiniEngineUnity
 						{
 							//Debug.Log("Found matched part: " + oldMatchedPart.name);
 
-							List<HEU_ObjectInstanceInfo> sourceObjectInstanceInfos = null;
-							if (bObjectInstancer)
-							{
-								// ProcessPart will clear out the object instances, so hence why
-								// we keep a copy here, then restore after processing the parts.
-								sourceObjectInstanceInfos = oldMatchedPart.GetObjectInstanceInfos();
-							}
+							// ProcessPart will clear out the object instances, so hence why
+							// we keep a copy here, then restore after processing the parts.
+							// Note that at this point we don't know if the the new part is an
+							// instancer or not (e.g. object, attribute instancer).
+							List<HEU_ObjectInstanceInfo> sourceObjectInstanceInfos = oldMatchedPart.GetObjectInstanceInfos();
 
 							// Clear out old generated data
 							oldMatchedPart.ClearGeneratedData();
@@ -276,7 +274,7 @@ namespace HoudiniEngineUnity
 
 							ProcessPart(session, i, ref partInfo, ref part);
 
-							if (part != null && bObjectInstancer && sourceObjectInstanceInfos != null)
+							if (sourceObjectInstanceInfos != null && part != null && (bObjectInstancer || part.IsAttribInstancer()))
 							{
 								// Set object instances from old part into new. This keeps the user set object inputs around.
 								part.SetObjectInstanceInfos(sourceObjectInstanceInfos);
