735bb8a54e4da9d2883fd6b7ea2fe3c34df53b01
Seelan Vamatheva <seelanv@sidefx.com>
2020-03-05 09:55:56 -0500

    Backports of r352926, r352928
    Handling session loading gracefully now.

--- Plugins/HoudiniEngineUnity/Scripts/Sessions/HEU_SessionManager.cs
+++ Plugins/HoudiniEngineUnity/Scripts/Sessions/HEU_SessionManager.cs
@@ -189,22 +189,29 @@ namespace HoudiniEngineUnity
             {
                 if (sessionData != null)
                 {
-                    // Create session based on type
-                    HEU_SessionBase sessionBase = CreateSessionFromType(sessionData.SessionClassType);
-                    if (sessionBase != null)
-                    {
-                        sessionBase.SetSessionData(sessionData);
-                        _sessionMap.Add(sessionData.SessionID, sessionBase);
+					try
+					{
+						// Create session based on type
+						HEU_SessionBase sessionBase = CreateSessionFromType(sessionData.SessionClassType);
+						if (sessionBase != null)
+						{
+							sessionBase.SetSessionData(sessionData);
+							_sessionMap.Add(sessionData.SessionID, sessionBase);
 
-                        if (sessionData.IsDefaultSession)
-                        {
-                            _defaultSession = sessionBase;
-                        }
 
-                        // Find assets in scene with session ID. Check if valid and reset those that aren't.
+							if (sessionData.IsDefaultSession)
+							{
+								_defaultSession = sessionBase;
+							}
 
-                    }
-                }
+							// Find assets in scene with session ID. Check if valid and reset those that aren't.
+						}
+					}
+					catch (System.Exception ex)
+					{
+						Debug.LogWarningFormat("Loading session with ID {0} failed with {1}. Ignoring the session.", sessionData.SessionID, ex.ToString());
+					}
+				}
             }
 
             InternalValidateSceneAssets();
