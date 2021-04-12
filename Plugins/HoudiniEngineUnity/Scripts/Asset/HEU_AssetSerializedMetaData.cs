a0f43516f44bffd022e1f88bbf834abaf9de4827
Anthony Tu <anthonyt@sidefx.com>
2021-04-12 10:33:14 -0400

    Fixed various issues regarding undoing the deletion of an HoudiniAssetRoot gameObject.

---
+++ Plugins/HoudiniEngineUnity/Scripts/Asset/HEU_AssetSerializedMetaData.cs
@@ -0,0 +1,41 @@
+﻿/*
+* Copyright (c) <2020> Side Effects Software Inc.
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+* 1. Redistributions of source code must retain the above copyright notice,
+*    this list of conditions and the following disclaimer.
+*
+* 2. The name of Side Effects Software may not be used to endorse or
+*    promote products derived from this software without specific prior
+*    written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY SIDE EFFECTS SOFTWARE "AS IS" AND ANY EXPRESS
+* OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+* OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN
+* NO EVENT SHALL SIDE EFFECTS SOFTWARE BE LIABLE FOR ANY DIRECT, INDIRECT,
+* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+* LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+* OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+* LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+* NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+* EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace HoudiniEngineUnity
+{
+    // Super hacky way to force save data when access is limited such as undo deletion events
+    public class HEU_AssetSerializedMetaData : ScriptableObject
+    {
+	[SerializeField]
+	private bool _softDeleted = false;
+	public bool SoftDeleted { get { return _softDeleted; } set { _softDeleted = value; } }
+    }
+}
