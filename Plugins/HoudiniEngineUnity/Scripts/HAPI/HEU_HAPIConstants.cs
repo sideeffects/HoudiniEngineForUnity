5a7c215fa77c233cb75d53034419037e5c368939
Anthony Tu <anthonyt@sidefx.com>
2021-06-22 10:48:12 -0400

    Bumping to Houdini Version: 19.0.294 Houdini Engine Version: 4.1 (API: 11)

--- Plugins/HoudiniEngineUnity/Scripts/HAPI/HEU_HAPIConstants.cs
+++ Plugins/HoudiniEngineUnity/Scripts/HAPI/HEU_HAPIConstants.cs
@@ -39,9 +39,68 @@ using UnityEngine;
 
 namespace HoudiniEngineUnity
 {
+    using HAPI_Bool = System.Boolean;
+    using HAPI_UInt8 = System.Byte;
+    using HAPI_Int8 = System.SByte;
+    using HAPI_Int16 = System.Int16;
+    using HAPI_Int64 = System.Int64;
+    using HAPI_ProcessId = System.Int32;
+    using HAPI_SessionId = System.Int64;
+    using HAPI_StringHandle = System.Int32;
+    using HAPI_AssetLibraryId = System.Int32;
+    using HAPI_NodeId = System.Int32;
+    using HAPI_ParmId = System.Int32;
+    using HAPI_PartId = System.Int32;
+    using HAPI_PDG_WorkitemId = System.Int32;
+    using HAPI_PDG_GraphContextId = System.Int32;
+    using HAPI_HIPFileId = System.Int32;
+    using HAPI_ErrorCodeBits = System.Int32;
+    using HAPI_NodeTypeBits = System.Int32;
+    using HAPI_NodeFlagsBits = System.Int32;
 
     public static class HEU_HAPIConstants
     {
+        public const int HAPI_POSITION_VECTOR_SIZE = 3;
+        public const int HAPI_SCALE_VECTOR_SIZE = 3;
+        public const int HAPI_SHEAR_VECTOR_SIZE = 3;
+        public const int HAPI_NORMAL_VECTOR_SIZE = 3;
+        public const int HAPI_QUATERNION_VECTOR_SIZE = 4;
+        public const int HAPI_EULER_VECTOR_SIZE = 3;
+        public const int HAPI_UV_VECTOR_SIZE = 2;
+        public const int HAPI_COLOR_VECTOR_SIZE = 4;
+        public const int HAPI_CV_VECTOR_SIZE = 4;
+        public const int HAPI_PRIM_MIN_VERTEX_COUNT = 1;
+        public const int HAPI_PRIM_MAX_VERTEX_COUNT = 16;
+        public const int HAPI_INVALID_PARM_ID = -1;
+        public const string HAPI_ATTRIB_POSITION = "P";
+        public const string HAPI_ATTRIB_UV = "uv";
+        public const string HAPI_ATTRIB_UV2 = "uv2";
+        public const string HAPI_ATTRIB_NORMAL = "N";
+        public const string HAPI_ATTRIB_TANGENT = "tangentu";
+        public const string HAPI_ATTRIB_TANGENT2 = "tangentv";
+        public const string HAPI_ATTRIB_COLOR = "Cd";
+        public const string HAPI_ATTRIB_NAME = "name";
+        public const string HAPI_ATTRIB_INSTANCE = "instance";
+        public const string HAPI_UNGROUPED_GROUP_NAME = "__ungrouped_group";
+        public const string HAPI_RAW_FORMAT_NAME = "HAPI_RAW";
+        public const string HAPI_PNG_FORMAT_NAME = "PNG";
+        public const string HAPI_JPEG_FORMAT_NAME = "JPEG";
+        public const string HAPI_BMP_FORMAT_NAME = "Bitmap";
+        public const string HAPI_TIFF_FORMAT_NAME = "TIFF";
+        public const string HAPI_TGA_FORMAT_NAME = "Targa";
+        public const string HAPI_DEFAULT_IMAGE_FORMAT_NAME = HAPI_PNG_FORMAT_NAME;
+        public const string HAPI_GLOBAL_NODES_NODE_NAME = "GlobalNodes";
+        public const string HAPI_ENV_HIP = "HIP";
+        public const string HAPI_ENV_JOB = "JOB";
+        public const string HAPI_ENV_CLIENT_NAME = "HAPI_CLIENT_NAME";
+        public const string HAPI_CACHE_COP_COOK = "COP Cook Cache";
+        public const string HAPI_CACHE_COP_FLIPBOOK = "COP Flipbook Cache";
+        public const string HAPI_CACHE_IMAGE = "Image Cache";
+        public const string HAPI_CACHE_OBJ = "Object Transform Cache";
+        public const string HAPI_CACHE_GL_TEXTURE = "OpenGL Texture Cache";
+        public const string HAPI_CACHE_GL_VERTEX = "OpenGL Vertex Cache";
+        public const string HAPI_CACHE_SOP = "SOP Cache";
+        public const string HAPI_CACHE_VEX = "VEX File Cache";
 
     }
 
