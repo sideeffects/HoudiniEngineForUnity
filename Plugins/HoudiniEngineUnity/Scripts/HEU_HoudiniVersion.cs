6da3a90e603600e7dd3af97dc1bd78a5555eb219
Anthony Tu <anthonyt@sidefx.com>
2021-11-19 10:31:46 -0500

    Bumping to Houdini Version: 19.5.74 Houdini Engine Version: 4.2 (API: 7)

--- Plugins/HoudiniEngineUnity/Scripts/HEU_HoudiniVersion.cs
+++ Plugins/HoudiniEngineUnity/Scripts/HEU_HoudiniVersion.cs
@@ -41,10 +41,10 @@ namespace HoudiniEngineUnity
         {
                 public const int HOUDINI_MAJOR			= 19;
                 public const int HOUDINI_MINOR			= 5;
-                public const int HOUDINI_BUILD			= 73;
+                public const int HOUDINI_BUILD			= 74;
                 public const int HOUDINI_PATCH			= 0;
 
-                public const string HOUDINI_VERSION_STRING = "19.5.73";
+                public const string HOUDINI_VERSION_STRING = "19.5.74";
 
                 public const int HOUDINI_ENGINE_MAJOR	= 4;
                 public const int HOUDINI_ENGINE_MINOR	= 2;
