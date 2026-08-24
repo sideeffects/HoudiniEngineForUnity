b204004d720939b275b16d29691e3faadc20aa7e
Damien Pernuit <damienp@sidefx.com>
2026-08-24 13:12:28 -0400

    Bumping to Houdini Version: 22.0.422 Houdini Engine Version: 9.0 (API: 0)

--- Plugins/HoudiniEngineUnity/Scripts/HEU_HoudiniVersion.cs
+++ Plugins/HoudiniEngineUnity/Scripts/HEU_HoudiniVersion.cs
@@ -41,10 +41,10 @@ namespace HoudiniEngineUnity
         {
                 public const int HOUDINI_MAJOR			= 22;
                 public const int HOUDINI_MINOR			= 0;
-                public const int HOUDINI_BUILD			= 415;
+                public const int HOUDINI_BUILD			= 422;
                 public const int HOUDINI_PATCH			= 0;
 
-                public const string HOUDINI_VERSION_STRING = "22.0.415";
+                public const string HOUDINI_VERSION_STRING = "22.0.422";
 
                 public const int HOUDINI_ENGINE_MAJOR	= 9;
                 public const int HOUDINI_ENGINE_MINOR	= 0;
